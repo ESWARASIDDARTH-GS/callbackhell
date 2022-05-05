e4afc7f7695bec24ecf62d4a6d17688fa436f831
SiDdArTh <98006478+ESWARASIDDARTH-GS@users.noreply.github.com>
2022-05-05 15:18:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(()=>{
+    document.querySelector(".countdown").innerText=9
+}, 1000)
+setTimeout(()=>{
+    document.querySelector(".countdown").innerText=8
+}, 2000)
+setTimeout(()=>{
+    document.querySelector(".countdown").innerText=7
+}, 3000)
+setTimeout(()=>{
+    document.querySelector(".countdown").innerText=6
+}, 4000)
+setTimeout(()=>{
+    document.querySelector(".countdown").innerText=5
+}, 5000)
+setTimeout(()=>{
+    document.querySelector(".countdown").innerText=4
+}, 6000)
+setTimeout(()=>{
+    document.querySelector(".countdown").innerText=3
+}, 7000)
+setTimeout(()=>{
+    document.querySelector(".countdown").innerText=2
+}, 8000)
+setTimeout(()=>{
+    document.querySelector(".countdown").innerText=1
+}, 9000)
+setTimeout(()=>{
+    document.querySelector(".countdown").innerText= 0
+}, 10000)
+setTimeout(()=>{
+    document.querySelector(".countdown").innerText="Happy independence day"
+}, 11000)
